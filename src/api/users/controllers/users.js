c9505720e6c6e2d3113328d1af5ad003e17dabb1
vanja-veapi <vanja.veapi@gmail.com>
2024-09-25 22:07:03 +0200

    enabled search query by username, first, lastname and email

--- src/api/users/controllers/users.js
+++ src/api/users/controllers/users.js
@@ -2,26 +2,44 @@
 
 module.exports = {
   async find(ctx) {
-    const { page = 1, pageSize = 10 } = ctx.query;
+    const { page = 1, pageSize = 10, search = "" } = ctx.query;
 
     // Konvertovanje page i pageSize u brojeve
     const pageNumber = parseInt(page, 10);
     const limit = parseInt(pageSize, 10);
     const start = (pageNumber - 1) * limit;
 
-    // Pribavljanje korisnika uz paginaciju
+    // Pribavljanje korisnika uz paginaciju i pretragu
     const users = await strapi.entityService.findMany(
       "plugin::users-permissions.user",
       {
         limit,
         start,
         populate: "*", // Ako želiš da popuniš sve povezane entitete
+        filters: {
+          $or: [
+            { username: { $containsi: search } },
+            { firstName: { $containsi: search } },
+            { lastName: { $containsi: search } },
+            { email: { $containsi: search } },
+          ],
+        },
       }
     );
 
-    // Ukupan broj korisnika
+    // Ukupan broj korisnika na osnovu pretrage
     const totalUsers = await strapi.entityService.count(
-      "plugin::users-permissions.user"
+      "plugin::users-permissions.user",
+      {
+        filters: {
+          $or: [
+            { username: { $containsi: search } },
+            { firstName: { $containsi: search } },
+            { lastName: { $containsi: search } },
+            { email: { $containsi: search } },
+          ],
+        },
+      }
     );
 
     // Broj stranica
